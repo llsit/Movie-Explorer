410fb39dedacaf8df99bcc4ba978fb573b03fb10
Nuttapong <thenuts53@gmail.com>
2025-01-29 21:45:50 +0700

    - add gradle conversion plugins

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    `kotlin-dsl`
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidLibraryComposeConventionPlugin") {
+            id = "gradlePlugins.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+    }
+}
